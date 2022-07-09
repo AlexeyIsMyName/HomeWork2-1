e94480ce27e7c7ce09d7becc686d7314c66ccfba
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-09 15:25:19 +0300

    Initial Commit

---
+++ HomeWork2-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2-1
+//
+//  Created by ALEKSEY SUSLOV on 09.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
